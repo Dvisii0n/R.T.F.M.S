abe822871aca38804eca7545762dfc9c4a576979
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-04-24 19:43:12 -0700

    se agregaron rutas iniciales y cors

--- flask/app.py
+++ flask/app.py
@@ -1,55 +1,47 @@
-from ultralytics import YOLO
-import cv2
 from flask import Flask, Response
+from flask_cors import CORS
+import app_services
+from detection import generate_frames
+from utils import require_auth
 
 app = Flask(__name__)
+CORS(app)
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
-
-model_path = "test_model.pt" 
-class_names = ["person"]
+base_url = "/api/v1"
 
 
-def run_yolo(img, model_path, class_names):
-
-    model = YOLO(model_path)
-    results = model(img, stream=True)
-
-    for result in results:
-        boxes = result.boxes
-
-        for box in boxes:
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 3)
-
-            cls = int(box.cls[0])
-
-            org = [x1, y1]
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            fontScale = 1
-            color = (0, 0, 255)
-            thickness = 2
-
-            cv2.putText(img, class_names[cls], org, font, fontScale, color, thickness)
-
-
-def generate_frames():
-    while True:
-        success, img = cap.read()
-
-        run_yolo(img, model_path, class_names)
-        _, buffer = cv2.imencode(".jpg", img)
-        frame_bytes = buffer.tobytes()
-
-        yield (b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + frame_bytes + b"\r\n")
-
-
-@app.route("/")
+@app.route(f"{base_url}/stream")
 def stream():
     return Response(
         generate_frames(), mimetype="multipart/x-mixed-replace; boundary=frame"
     )
+
+
+@app.get(f"{base_url}/users")
+@require_auth
+def users_get():
+    return app_services.get_users()
+
+
+@app.post(f"{base_url}/users")
+def users_post():
+    return app_services.create_user()
+
+
+@app.post(f"{base_url}/login")
+def login_post():
+    return app_services.login()
+
+
+# crear alerta
+# ver alertas
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return {"error": "404 Not Found"}, 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return {"error": "500 Internal server error"}, 500
