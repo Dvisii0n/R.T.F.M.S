51e869ed530b51ddd0f6558f2a22b7dc74117901
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-05-15 13:26:24 -0700

    se agrego la funcionalidad de mandar eventos con detecciones al front end

--- flask/detection.py
+++ flask/detection.py
@@ -1,12 +1,16 @@
 from ultralytics import YOLO
 import cv2
+import urllib.request
+import json
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
-model_path = "test_model.pt"
-class_names = ["person"]
+model_path = "fire_model.pt"
+class_names = ["fire"]
+
+default_location = {"lat": 23.2435, "lon": -106.4294}
 
 model = YOLO(model_path)
 
@@ -16,6 +20,8 @@ def run_yolo(img, class_names):
 
     for result in results:
         boxes = result.boxes
+
+        send_detection_event(len(boxes))
 
         for box in boxes:
             x1, y1, x2, y2 = box.xyxy[0]
@@ -43,3 +49,44 @@ def generate_frames():
         frame_bytes = buffer.tobytes()
 
         yield (b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + frame_bytes + b"\r\n")
+
+
+def send_detection_event(num_detections):
+    risk = get_risk(num_detections)
+    data = json.dumps(
+        {
+            "type": "DETECCION",
+            "num_detecciones": num_detections,
+            "ubicacion": default_location,
+            "nivel_riesgo": risk,
+        }
+    ).encode("utf-8")
+    req = urllib.request.Request(
+        "http://localhost:8081/send_event",
+        data=data,
+        headers={"Content-Type": "application/json"},
+    )
+    try:
+        with urllib.request.urlopen(req) as response:
+            print("Response:", response.read().decode())
+    except Exception as e:
+        print("Error:", e)
+
+
+def get_risk(num_detections):
+    if num_detections == 0:
+        return "SEGURO"
+
+    if num_detections == 1:
+        return "BAJO"
+
+    if num_detections == 2:
+        return "MEDIO"
+
+    if num_detections >= 3:
+        return "ALTO"
+
+
+# for i in range(1, 30):
+#     img = cv2.imread("test_fuego.png")
+#     run_yolo(img, class_names)
